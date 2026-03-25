9f33e8a3bfcfbf41f83baa98e0043149917bdae9
mflowers <24275214+digital-grease@users.noreply.github.com>
2026-03-25 15:34:01 -0500

    update dependencies

--- app/src/main/java/com/fauxx/ui/screens/OnboardingScreen.kt
+++ app/src/main/java/com/fauxx/ui/screens/OnboardingScreen.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.fauxx.data.querybank.CategoryPool
 import com.fauxx.targeting.layer1.AgeRange
 import com.fauxx.targeting.layer1.Gender
